725ba1d34a850b61867720983c1e4c41b5a30295
Tlhohonolofatso Semelane <tlhohonolofatsosemelane2@gmail.com>
2026-07-13 15:42:57 +0200

    Add internship profile Dart exercise

---
+++ 01_basic_syntax/internship_profile.dart
@@ -0,0 +1,19 @@
+void main() {
+  String internName = 'Tlhohonolofatso Semelane';
+  String organization = 'Academic Bridge';
+  String team = 'Mobile App Development Team';
+  String currentFocus = 'Dart and Flutter';
+  int completedModules = 3;
+  int remainingModules = 2;
+  bool isLearningGitHub = true;
+
+  print('Internship Learning Profile');
+  print('----------------------------');
+  print('Name: $internName');
+  print('Organization: $organization');
+  print('Team: $team');
+  print('Current Focus: $currentFocus');
+  print('Completed Coursera Modules: $completedModules');
+  print('Remaining Coursera Modules: $remainingModules');
+  print('Learning GitHub: $isLearningGitHub');
+}
