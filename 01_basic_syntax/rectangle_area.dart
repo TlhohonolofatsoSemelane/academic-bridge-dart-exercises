3ecb1aa1af15e0d96335e3b3b48bad810689fdb7
Tlhohonolofatso Semelane <tlhohonolofatsosemelane2@gmail.com>
2026-07-13 16:09:44 +0200

    Update rectangle area exercise

---
+++ 01_basic_syntax/rectangle_area.dart
@@ -0,0 +1,10 @@
+void main() {
+  int length = 12;
+  int width = 6;
+
+  int area = length * width;
+
+  print('Length: $length');
+  print('Width: $width');
+  print('Area of rectangle: $area');
+}
