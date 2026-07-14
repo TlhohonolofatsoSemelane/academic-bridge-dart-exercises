42955b4108e7dd579d6332d30eafefcf16921a9f
Tlhohonolofatso Semelane <tlhohonolofatsosemelane2@gmail.com>
2026-07-14 19:37:09 +0200

    Adding my course variables exercise

---
+++ 02_variables_data_types/my_course_variables.dart
@@ -0,0 +1,11 @@
+void main() {
+  var course = 'Flutter course';
+  int modulesDone = 3;
+  int totalModules = 5;
+  bool stillPracticing = true;
+
+  print(course);
+  print(modulesDone);
+  print(totalModules);
+  print(stillPracticing);
+}
