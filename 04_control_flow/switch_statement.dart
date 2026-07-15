c0725dbfde3664d1a7050c99634f42b36803f3e7
Tlhohonolofatso Semelane <tlhohonolofatsosemelane2@gmail.com>
2026-07-15 19:46:39 +0200

    Add switch statement exercises

---
+++ 04_control_flow/switch_statement.dart
@@ -0,0 +1,17 @@
+void main() {
+  String currentTopic = 'Flutter';
+
+  switch (currentTopic) {
+    case 'Dart':
+      print('Focus Area: Programming fundamentals');
+      break;
+    case 'Flutter':
+      print('Focus Area: Mobile app user interface development');
+      break;
+    case 'GitHub':
+      print('Focus Area: Version control and project hosting');
+      break;
+    default:
+      print('Focus Area: General learning');
+  }
+}
