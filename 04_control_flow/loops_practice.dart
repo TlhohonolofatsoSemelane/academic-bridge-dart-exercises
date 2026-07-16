6306052d0a110611408fb7e2875ea1b5e1426f49
Tlhohonolofatso Semelane <tlhohonolofatsosemelane2@gmail.com>
2026-07-16 02:00:09 +0200

    Add loops practice exercise

---
+++ 04_control_flow/loops_practice.dart
@@ -0,0 +1,13 @@
+void main() {
+  List<String> modules = [
+    'Module 1',
+    'Module 2',
+    'Module 3',
+    'Module 4',
+    'Module 5'
+  ];
+
+  for (int i = 0; i < modules.length; i++) {
+    print('${modules[i]}: Learning progress item ${i + 1}');
+  }
+}
