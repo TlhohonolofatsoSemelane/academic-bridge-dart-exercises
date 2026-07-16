4203ba1c26e0b7ba7504db54eccb4e4765fd3183
Tlhohonolofatso Semelane <tlhohonolofatsosemelane2@gmail.com>
2026-07-16 12:57:25 +0200

    Add grade calculator exercises

---
+++ 10_mini_challenges/grade_calculator.dart
@@ -0,0 +1,15 @@
+void main() {
+  int mark = 76;
+
+  if (mark >= 80) {
+    print('Grade: A');
+  } else if (mark >= 70) {
+    print('Grade: B');
+  } else if (mark >= 60) {
+    print('Grade: C');
+  } else if (mark >= 50) {
+    print('Grade: D');
+  } else {
+    print('Grade: F');
+  }
+}
